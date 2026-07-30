662829ad80f021d0b54c96ad4a36fcb59efa4559
Hansani-Maheshika <hansanihdhm21@gmail.com>
2026-07-30 15:03:39 +0530

    feat: complete backend setup and health API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
-import app from "./app.ts";
-import { env } from "./config/env.ts";
+import app from "./app.js";
+import { env } from "./config/env.js";
 
 const server = app.listen(env.PORT, () => {
   console.log(
